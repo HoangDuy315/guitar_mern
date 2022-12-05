bf8f8c99875a4f74ba9881b31b75d0f06200dbf0
thuanhaine <19521462@gm.uit.edu.vn>
2022-12-05 13:45:20 +0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ import ManagementUser from "./components/managementuser/ManagementUser";
 function App() {
   const [auth, setAuth] = useState(false);
   const [auth1, setAuth1] = useState(true);
-
+  console.log(auth1);
   const isLoggedIn = async () => {
     try {
       const res = await fetch("/auth", {
