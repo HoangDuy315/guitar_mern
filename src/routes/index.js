d1f91ccc499f15ba15917fc379ed8e64244fdfb7
DucManh75 <91884985+DucManh75@users.noreply.github.com>
2022-12-16 13:26:45 +0700

    signup form and terms

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@
 
 import {Login, Signup} from '../component/login_register';
 import { About, Contact, Books, Blog, Header, Home, Footer, QLUser, InforUser, QLProduct } from '../component';
+import Terms from '../component/TermsService/Terms';
 
 const publichRoutes = [
     { path: '/', component: Home },
     { path: '/login', component: Login, layout: null},
     { path: '/signup', component: Signup, layout: null},
+    { path: '/terms', component: Terms, layout: null},
 
 ]
 const  privateRoutes = [
