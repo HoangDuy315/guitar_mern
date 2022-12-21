1b109802ea3fa629c4b23cf32482857463ff15d3
DucManh75 <91884985+DucManh75@users.noreply.github.com>
2022-12-21 22:40:37 +0700

    update data

--- src/component/home/dataDefautl.js
+++ src/component/home/dataDefautl.js
@@ -9,26 +9,8 @@
     type: "Acoustic",
     description:"Guitar Suzuki SDG-6BK là mẫu đàn Acoustic, đây là loại đàn thông dụng và phổ biến dành cho lứa tuổi học sinh, sinh viên, người mới tập chơi guitar chọn lựa.",
   },
-  {
-    id:2,
-    name: 'Cordoba Protégé C1 Matiz - Coral',
-    urlImg: "https://upload-img-theta.vercel.app/guitar2.png",
-    quantity: 6,
-    price: 6800000,
-    color: "pink",
-    type: "Nylon",
-    description:`Mang âm nhạc của bạn trở nên sống động với đầy màu sắc với Protégé C1 Matiz! Mang nét thẩm mỹ mới mẻ của cây đàn guitar dây nylon, dòng Matiz kết hợp các điểm nhấn bằng gỗ thích và gỗ có tông màu truyền thống với 4 màu sắc bắt mắt: Aqua, Coral, Pale Sky và Classic Blue`,
-  },
-  {
-    id:3,
-    name: 'Taylor AD17E',
-    urlImg: 'https://upload-img-theta.vercel.app/guitar3.png',
-    quantity: 4,
-    price: 44000000,
-    color: "yellow",
-    type:"Taylor",
-    description: "Với loạt guitar acoustic do Mỹ chế tạo trong American Dream, Bob Taylor và Andy Powers đã mở ra một chương mới về giá trị và hiệu suất của Taylor",
-  }
+ 
+
 ];
 
 export {ListBookDefautl}
