91347dd2326aa01a87827336902e23490c1f2533
glong25 <92261979+glong25@users.noreply.github.com>
2022-12-22 21:45:15 +0700

    update staff

--- src/component/pages/blog/Blog.jsx
+++ src/component/pages/blog/Blog.jsx
@@ -14,10 +14,10 @@ function Blog() {
                     <p className="about-role">Member</p>
                 </li>
                 <li className="about-item">
-                    <img className="about-avatar" src={avatar} alt="Avatar"/>
+                    <img className="about-avatar-leader" src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t1.6435-9/70615256_2447394002164828_1028188705073922048_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=k05fwvwVxgYAX8nxBF9&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfBMMRuvfwjSMI8OeUVNw9lV9FOk8o5GhjJc6ex71gU-sQ&oe=63CBC938" alt="Avatar"/>
                     <h2 className="about-name">Lường Tiến Thuận Hải</h2>
                     <p className="about-desc">19521462</p>
-                    <p className="about-role">Leader</p>
+                    <p className="about-role-leader">Leader</p>
                 </li>
                 <li className="about-item">
                     <img className="about-avatar" src="https://scontent.fsgn1-1.fna.fbcdn.net/v/t1.6435-9/192277015_2966801990254649_1743026157593445143_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=btsieHd6oNkAX80BUD2&_nc_ht=scontent.fsgn1-1.fna&oh=00_AfBzSKt9pG5e51J9JxznANAdVeZvcWQBvHDMknbrgDczCQ&oe=63CB7D4F" alt="Avatar"/>
@@ -26,7 +26,7 @@ function Blog() {
                     <p className="about-role">Member</p>
                 </li>
                 <li className="about-item">
-                    <img className="about-avatar" src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/274179162_3199216190359548_4336456839941786998_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=njN5MYDmvOQAX8Qfyo2&_nc_ht=scontent.fsgn5-8.fna&oh=00_AfDvF2hqVIECFLbH-5XCkbBF_iPT9f20v9-D0w01PxX4uA&oe=63AA0D1E" alt="Avatar"/>
+                    <img className="about-avatar" src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/273205795_3184501488497685_413369274076755631_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=jPX1cWAKGUwAX-4tw-R&tn=0XSJxjKQk1pklyR3&_nc_ht=scontent.fsgn5-12.fna&oh=00_AfADzFzvuDf6Nt3Fyhjnm54V3Hit36hICfF2W2oNfaZJfg&oe=63A9A11B" alt="Avatar"/>
                     <h2 className="about-name">Nguyễn Hoàng long</h2>
                     <p className="about-desc">19521787</p>
                     <p className="about-role">Member</p>
